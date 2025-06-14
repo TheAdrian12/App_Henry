e0193d17e3a9b230ed038e0195b1855eea04099f
TheAdrian12 <ajac4183@gmail.com>
2025-06-14 14:17:47 -0500

    Implementación de vista para mostrar usuarios desde API
    Se implementó la pantalla Succes3 que muestra una lista de usuarios obtenidos dinámicamente desde un servicio HTTP. Los datos incluyen nombre, número, correo e imagen. Esta vista reemplaza los datos estáticos por contenido real obtenido desde el bloc.

--- contact_app/lib/features/home/Presentation/blocs/home_state.dart
+++ contact_app/lib/features/home/Presentation/blocs/home_state.dart
@@ -4,10 +4,16 @@ sealed class HomeState extends Equatable {
   const HomeState();
   
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
 final class HomeInitial extends HomeState {}
 final class HomeLoadInProgress extends HomeState{}
-final class HomeLoadSuccess extends HomeState{}
+final class HomeLoadSuccess extends HomeState{
+  final List<User> usuarios;
+   const HomeLoadSuccess(this.usuarios);
+  @override
+  List<Object?> get props => [usuarios];
+
+}
 final class HomeLoadFailure extends HomeState{}
--- contact_app/lib/features/home/Presentation/views/Succes_view.dart
+++ contact_app/lib/features/home/Presentation/views/Succes_view.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../../../data/models/user.dart';
+
 class Succes3 extends StatelessWidget {
-  const Succes3({Key? key}) : super(key: key);
+   final List<User> usuarios;
+
+   const Succes3({Key? key, required this.usuarios}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,45 +19,17 @@ class Succes3 extends StatelessWidget {
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
-      body: SingleChildScrollView( // <--- AQUI VA EL CAMBIO CLAVE
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              contactoItem(
-                nombre: 'Pablo Velaquez',
-                telefono: '+52 456 781 1549',
-                email: 'velasquez88@gmail.com',
-                imagen: 'https://st4.depositphotos.com/10638838/37791/v/450/depositphotos_377910952-stock-illustration-man-avatar-icon-flat-color.jpg',
-              ),
-              contactoItem(
-                nombre: 'Ana Torres',
-                telefono: '+52 456 852 1324',
-                email: 'anatorres54@gmail.com',
-                imagen: 'https://cdn-icons-png.flaticon.com/512/3362/3362093.png',
-              ),
-              contactoItem(
-                nombre: 'Miguel Herrera',
-                telefono: '+52 123 888 6987',
-                email: 'miguelitoher@gmail.com',
-                imagen: 'https://st4.depositphotos.com/10638838/37791/v/450/depositphotos_377910952-stock-illustration-man-avatar-icon-flat-color.jpg',
-              ),
-              contactoItem(
-                nombre: 'Cesar Paolo',
-                telefono: '+52 546 852 9999',
-                email: 'cesarpaolos@gmail.com',
-                imagen: 'https://st4.depositphotos.com/10638838/37791/v/450/depositphotos_377910952-stock-illustration-man-avatar-icon-flat-color.jpg',
-              ),
-              contactoItem(
-                nombre: 'Laura Martinez',
-                telefono: '+52 321 654 7777',
-                email: 'laruramartinez@gmail.com',
-                imagen: 'https://cdn-icons-png.flaticon.com/512/3362/3362093.png',
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemCount: usuarios.length,
+      itemBuilder: (context, index){
+        final user = usuarios[index];
+        return contactoItem(
+          nombre: user.nombre, 
+          telefono: user.numero, 
+          email: user.email, 
+          imagen: user.imagen);
+      })
     );
   }
 
